75d478fcf6e6ac851d8e0c3a6e7d8ff71bee7f4a
Pooja Prasad <pooja.prasad2@mindtree.com>
2021-03-09 11:13:20 +0530

    contact schema

--- server/server.js
+++ server/server.js
@@ -1,2 +1,31 @@
-let test="hi";
-let test2="hello";
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const dbConfig = require('./config/config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url,{ useUnifiedTopology: true }, {
+   useNewUrlParser: true
+}).then(() => {
+   console.log("Successfully connected to the database");
+}).catch(err => {
+   console.log('Could not connect to the database', err);
+   process.exit();
+});
+
+app.get('/', (req, res) => {
+   res.json({"message": "Welcome!"});
+});
+
+require('../server/routes/contactRoute.js')(app);
+
+app.listen(5000, () => {
+   console.log("Server is listening on port 5000");
+});

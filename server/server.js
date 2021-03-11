3bb29279e8d6df962489576c3d650077e547fa73
Pooja Prasad <pooja.prasad2@mindtree.com>
2021-03-11 12:11:09 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,9 +40,19 @@ const mongoose = require('mongoose');
 const app = express();
 // require('dotenv').config();
 
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Credentials',true);
+    res.header('Access-Control-Allow-Origin','*') ; 
+    res.setHeader('Access-Control-Allow-Headers',"Origin, X-Requested-with,Content-Type,Accept,Authorization");
+    res.setHeader('Access-Control-Allow-Headers','record-count,my-token,x-auth');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    next();
+})
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());

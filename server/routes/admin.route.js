4584ee3949fa66df94e6856c1fa2302c4e6f40b7
manurag173@gmail.com <Anurag.Mishra2@mindtree.com>
2021-03-26 00:17:22 +0530

    pending and completed

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 const { checker, deleteUser,
-    LogOut, deleteRepresentative, viewActivities, postCredentials, verifyRepresentative, viewUsers, viewRepresentatives, viewContacts, viewUsersById, viewRepresentativesById} = require('../controllers/admin.controller')
+    LogOut, deleteRepresentative, viewActivities, postCredentials, verifyRepresentative, viewUsers, viewRepresentatives, viewContacts, viewPendingActivities, viewCompletedActivities, viewRepresentativesById} = require('../controllers/admin.controller')
 
 router.get('/', checker);
 
 router.get('/users', viewUsers);
 router.get('/representatives', viewRepresentatives);
-router.get('/users/:id', viewUsersById);
+
+router.get('/user/pending/:id', viewPendingActivities);
+router.get('/user/completed/:id', viewCompletedActivities);
+
 router.get('/representative/:id', viewRepresentativesById);
 router.get('/contacts', viewContacts);
 router.get('/activity/:id', viewActivities);
